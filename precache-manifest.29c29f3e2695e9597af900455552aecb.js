86df22681d4f73d7a04267304740f1dd02d9701c
WebTarantul <tarstar1990@gmail.com>
2020-01-13 19:59:44 +0200

    Updates

---
+++ precache-manifest.29c29f3e2695e9597af900455552aecb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "550d74bcee3c819773f8366734350135",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "1beab6dce9fca4eae9b7",
+    "url": "/todo-list/static/css/main.2456a0a0.chunk.css"
+  },
+  {
+    "revision": "52aa68427cfd6a3e0e7d",
+    "url": "/todo-list/static/js/2.2ace64c4.chunk.js"
+  },
+  {
+    "revision": "1beab6dce9fca4eae9b7",
+    "url": "/todo-list/static/js/main.995e9c93.chunk.js"
+  },
+  {
+    "revision": "ca797cc406ded84cf733",
+    "url": "/todo-list/static/js/runtime~main.41b69a58.js"
+  }
+]);
